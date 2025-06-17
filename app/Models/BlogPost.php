f3e3cee19d92d2e1700ebe3ad0350872924df1c5
nataliaradcuk <nataliaradcuk68@gmail.com>
2025-06-16 22:05:41 -0400

    Lab 10: Add and delete blog posts

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class BlogPost extends Model
 {
     use HasFactory, SoftDeletes;
+    const UNKNOWN_USER = 1;
     protected $fillable
         = [
             'title',
@@ -18,7 +19,7 @@ class BlogPost extends Model
             'content_raw',
             'is_published',
             'published_at',
-            'user_id',
+
         ];
 
     /**
