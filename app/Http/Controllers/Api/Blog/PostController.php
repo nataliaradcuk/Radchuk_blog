9786a103b70f0eb8642c412564819c438d235d92
nataliaradcuk <nataliaradcuk68@gmail.com>
2025-06-22 17:54:55 -0400

    Lab 15: Vue, API

---
+++ app/Http/Controllers/Api/Blog/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Blog;
+
+use App\Http\Controllers\Controller;
+use App\Models\BlogPost;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = BlogPost::with(['user', 'category'])->get();
+        return response()->json(['data' => $posts]);
+    }
+}
