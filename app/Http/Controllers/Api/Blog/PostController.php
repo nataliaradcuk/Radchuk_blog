3c150a327522a40a159cf9b9e3216f5983d15959
nataliaradcuk <nataliaradcuk68@gmail.com>
2025-06-22 19:50:55 -0400

    16. Nuxt, API, Blog Post View

--- app/Http/Controllers/Api/Blog/PostController.php
+++ app/Http/Controllers/Api/Blog/PostController.php
@@ -12,4 +12,10 @@ class PostController extends Controller
         $posts = BlogPost::with(['user', 'category'])->get();
         return response()->json(['data' => $posts]);
     }
+
+    public function show($id)
+    {
+        $post = BlogPost::with(['user', 'category'])->findOrFail($id);
+        return response()->json(['data' => $post]);
+    }
 }
